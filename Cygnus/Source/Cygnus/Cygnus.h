eb7601c05c1441fbf168e23a8feb803069be0da7
Rasmit Devkota <alientechgamig@hotmail.com>
2021-04-04 19:17:56 -0400

    Lots, lots, and lots of changes!

--- Cygnus/Source/Cygnus/Cygnus.h
+++ Cygnus/Source/Cygnus/Cygnus.h
@@ -1,5 +1,9 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #pragma once
 
 #include "CoreMinimal.h"
+
+#include "Runtime/UMG/Public/UMG.h"
+#include "Runtime/UMG/Public/UMGStyle.h"
+#include "Runtime/UMG/Public/Blueprint/UserWidget.h"
+#include "Runtime/UMG/Public/Slate/SObjectWidget.h"
+#include "Runtime/UMG/Public/IUMGModule.h"
