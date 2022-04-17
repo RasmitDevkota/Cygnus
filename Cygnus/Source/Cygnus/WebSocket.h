26bf312b328b9220cfa0a7241d611182d235b35b
Rasmit Devkota <alientechgamig@hotmail.com>
2022-04-17 14:08:36 -0400

    removed 4.27

---
+++ Cygnus/Source/Cygnus/WebSocket.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "WebSocket.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CYGNUS_API UWebSocket : public UObject
+{
+	GENERATED_BODY()
+};
